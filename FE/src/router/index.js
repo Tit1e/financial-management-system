15eeaa70167978a92bf610af165c846c9bc023d9
Tit1e <evollyone@outlook.com>
2020-03-01 21:33:40 +0800

    路由增加

--- FE/src/router/index.js
+++ FE/src/router/index.js
@@ -1,23 +1,45 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Layout from '@/views/layout'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    redirect: 'login',
+    component: () => import('@/views/login/index.vue')
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    path: '/login',
+    name: 'login',
+    component: () => import('@/views/login/index.vue')
+  },
+  {
+    path: '/',
+    component: Layout,
+    redirect: 'dashboard',
+    children: [
+      {
+        path: '/dashboard',
+        name: 'dashboard',
+        component: () => import('@/views/dashboard/index.vue'),
+        meta: { title: '首页', icon: 'iconfont icon-jijin' }
+      },
+      {
+        path: '/detail',
+        name: 'detail',
+        component: () => import('@/views/detail/index.vue'),
+        meta: { title: '收支明细', icon: 'iconfont icon-jijin' }
+      },
+      {
+        path: '/setting',
+        name: 'setting',
+        component: () => import('@/views/setting/index.vue'),
+        meta: { title: '系统设置', icon: 'iconfont icon-jijin' }
+      },
+    ]
+  },
 ]
 
 const router = new VueRouter({
