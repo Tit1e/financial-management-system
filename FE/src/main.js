c07b34ee7b4ce0a976a4daee9955554cb97fccbc
Tit1e <evollyone@outlook.com>
2020-03-08 15:37:15 +0800

    引入新组件

--- FE/src/main.js
+++ FE/src/main.js
@@ -9,11 +9,16 @@ import Notify from '@/utils/notify.js'
 import './directive'
 import '@/assets/base.scss'
 
+import ECharts from 'vue-echarts'
+// import 'echarts/lib/chart/bar'
+// import 'echarts/lib/component/tooltip'
+
 Vue.config.productionTip = false
 
 Vue.prototype.$Notify = Notify
 Vue.prototype.$dayjs = dayjs
 
+Vue.component('v-chart', ECharts)
 
 new Vue({
   router,
